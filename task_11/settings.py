6cf8408d48afd17836b5c1f6f12f992f2815d8b0
Noura Al-Rashidi <nouraal-rashidi@Nouras-MacBook.local>
2018-07-18 17:38:51 +0300

    prepared django project for deployment

--- task_11/settings.py
+++ task_11/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tnvhv(22@t1#1!f9$&6gl#0-&9(symuxb$=x_2h6zkg*tdi4iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '46.101.115.131'
+]
 
 
 # Application definition
@@ -79,10 +81,19 @@ WSGI_APPLICATION = 'task_11.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'aa063a56ddf38e85caf70aa335b7ca55',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 
 
